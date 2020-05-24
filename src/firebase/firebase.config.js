902807756122f9d539838d609808792376bbce0e
Gouze <gouze@me.com>
2020-05-24 17:32:24 +0200

    add config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: 'AIzaSyApFEcbt2z1RGnkqb8dqI1eomSGx_7Bi8U',
+  authDomain: 'react-ecommerce-c4e1d.firebaseapp.com',
+  databaseURL: 'https://react-ecommerce-c4e1d.firebaseio.com',
+  projectId: 'react-ecommerce-c4e1d',
+  storageBucket: 'react-ecommerce-c4e1d.appspot.com',
+  messagingSenderId: '1097549762814',
+  appId: '1:1097549762814:web:df3b34d6250c70697995af',
+  measurementId: 'G-3PGH8SP0Z3',
+};
